bdc7b31ec9413910fe58d21d9cba79a866c8a40c
christian chiappa <christian.chiappa@liferay.com>
2021-11-30 18:03:38 +0100

    COMMERCE-7979 update warehouse DTO converter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-inventory-impl/src/main/java/com/liferay/headless/commerce/admin/inventory/internal/dto/v1_0/WarehouseDTOConverter.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-inventory-impl/src/main/java/com/liferay/headless/commerce/admin/inventory/internal/dto/v1_0/WarehouseDTOConverter.java
@@ -17,6 +17,7 @@ package com.liferay.headless.commerce.admin.inventory.internal.dto.v1_0;
 import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
 import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseService;
 import com.liferay.headless.commerce.admin.inventory.dto.v1_0.Warehouse;
+import com.liferay.headless.commerce.core.util.LanguageUtils;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 
@@ -53,7 +54,8 @@ public class WarehouseDTOConverter
 				city = commerceInventoryWarehouse.getCity();
 				countryISOCode =
 					commerceInventoryWarehouse.getCountryTwoLettersISOCode();
-				description = commerceInventoryWarehouse.getDescription();
+				description = LanguageUtils.getLanguageIdMap(
+					commerceInventoryWarehouse.getDescriptionMap());
 				externalReferenceCode =
 					commerceInventoryWarehouse.getExternalReferenceCode();
 				id =
@@ -62,7 +64,8 @@ public class WarehouseDTOConverter
 				latitude = commerceInventoryWarehouse.getLatitude();
 				longitude = commerceInventoryWarehouse.getLongitude();
 				mvccVersion = commerceInventoryWarehouse.getMvccVersion();
-				name = commerceInventoryWarehouse.getName();
+				name = LanguageUtils.getLanguageIdMap(
+					commerceInventoryWarehouse.getNameMap());
 				regionISOCode =
 					commerceInventoryWarehouse.getCommerceRegionCode();
 				street1 = commerceInventoryWarehouse.getStreet1();
