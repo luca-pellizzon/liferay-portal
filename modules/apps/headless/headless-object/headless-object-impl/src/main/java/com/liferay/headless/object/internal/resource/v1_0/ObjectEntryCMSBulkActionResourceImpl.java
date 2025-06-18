6e0427dd0b3d7b87fae1fd2b2df7be58c2b29d6b
Crescenzo Rega <crescenzo.rega@liferay.com>
2025-06-18 17:19:36 +0200

    Adds delete and permission management

--- modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryCMSBulkActionResourceImpl.java
+++ modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryCMSBulkActionResourceImpl.java
@@ -12,14 +12,18 @@ import com.liferay.headless.object.dto.v1_0.ObjectEntryCMSBulkActionResponse;
 import com.liferay.headless.object.resource.v1_0.ObjectEntryCMSBulkActionResource;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.model.ObjectEntryFolder;
 import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryFolderLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.vulcan.permission.Permission;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,9 +49,9 @@ public class ObjectEntryCMSBulkActionResourceImpl
 			ObjectEntryCMSBulkActionRequest objectEntryCMSBulkActionRequest)
 		throws Exception {
 
-		Long[] entryIds = objectEntryCMSBulkActionRequest.getIds();
+		Long[] ids = objectEntryCMSBulkActionRequest.getIds();
 
-		if (ArrayUtil.isEmpty(entryIds)) {
+		if (ArrayUtil.isEmpty(ids)) {
 
 			// Retrieve results from original query + filters + search
 
@@ -55,49 +59,88 @@ public class ObjectEntryCMSBulkActionResourceImpl
 
 		// Organize IDs by their ObjectDefinition
 
-		Map<Long, List<Long>> entriesIdByObjectDefinition = new HashMap<>();
+		Map<String, Map<Long, List<Long>>> map = new HashMap<>();
 
-		for (long entryId : entryIds) {
-			ObjectEntry objectEntry = _objectEntryLocalService.getObjectEntry(
-				entryId);
+		for (long id : ids) {
+			ObjectEntry objectEntry = _objectEntryLocalService.fetchObjectEntry(
+				id);
 
-			List<Long> idList = entriesIdByObjectDefinition.get(
-				objectEntry.getObjectDefinitionId());
-
-			if ((idList == null) || ListUtil.isEmpty(idList)) {
-				idList = new ArrayList<>();
-
-				entriesIdByObjectDefinition.put(
-					objectEntry.getObjectDefinitionId(), idList);
+			if (objectEntry != null) {
+				map.computeIfAbsent(
+					"ObjectEntry", key -> new HashMap<>()
+				).computeIfAbsent(
+					objectEntry.getObjectDefinitionId(),
+					objectEntryId -> new ArrayList<>()
+				).add(
+					id
+				);
 			}
+			else {
+				ObjectEntryFolder objectEntryFolder =
+					_objectEntryFolderLocalService.fetchObjectEntryFolder(id);
 
-			idList.add(entryId);
+				if (objectEntryFolder != null) {
+					map.computeIfAbsent(
+						"ObjectEntryFolder", key -> new HashMap<>()
+					).computeIfAbsent(
+						objectEntryFolder.getObjectEntryFolderId(),
+						key -> new ArrayList<>()
+					).add(
+						id
+					);
+				}
+			}
 		}
 
 		// Call the batch endpoint of each ObjectDefinition
 
 		List<Long> batchIds = new ArrayList<>();
 
-		switch (bulkActionName) {
-			case "remove":
-				batchIds.addAll(_removeEntries(entriesIdByObjectDefinition));
+		if (bulkActionName.equals("remove")) {
+			_removeEntries(map);
+		}
+		else if (bulkActionName.equals("permission")) {
+			_setPermissions(
+				map, objectEntryCMSBulkActionRequest.getPermissions());
 		}
 
 		// Return batch IDs to caller
 
-		ObjectEntryCMSBulkActionResponse objectEntryCMSBulkActionResponse = new ObjectEntryCMSBulkActionResponse();
-
-		objectEntryCMSBulkActionResponse.setIds(batchIds.toArray(new Long[0]));
-
-		return objectEntryCMSBulkActionResponse;
+		return new ObjectEntryCMSBulkActionResponse() {
+			{
+				setIds(() -> batchIds.toArray(new Long[0]));
+			}
+		};
 	}
 
-	private List<Long> _removeEntries(
-			Map<Long, List<Long>> entriesIdByObjectDefinition)
+	private Map<String, Object> _deleteImportTaskObject(
+			String className, List<Long> ids,
+			ImportTaskResource importTaskResource, String taskItemDelegateName)
 		throws Exception {
 
-		List<Long> batchIds = new ArrayList<>();
-		ImportTaskResource importTaskResource = _factory.create(
+		if (ListUtil.isEmpty(ids)) {
+			return Collections.emptyMap();
+		}
+
+		ImportTask importTask = importTaskResource.deleteImportTaskObject(
+			className, null, null,
+			ImportTask.ImportStrategy.ON_ERROR_CONTINUE.getValue(),
+			taskItemDelegateName,
+			transform(
+				ids,
+				id -> HashMapBuilder.put(
+					"id", id
+				).build()));
+
+		return HashMapBuilder.<String, Object>put(
+			"id", importTask.getId()
+		).put(
+			"processedItems", ids
+		).build();
+	}
+
+	private ImportTaskResource _getImportTaskResource() {
+		return _factory.create(
 		).httpServletRequest(
 			contextHttpServletRequest
 		).httpServletResponse(
@@ -107,29 +150,125 @@ public class ObjectEntryCMSBulkActionResourceImpl
 		).user(
 			contextUser
 		).build();
+	}
 
-		for (Map.Entry<Long, List<Long>> entries :
-				entriesIdByObjectDefinition.entrySet()) {
+	private Map<String, Object> _putImportTaskObject(
+			String className, List<Long> ids,
+			ImportTaskResource importTaskResource, Permission[] permissions,
+			String taskItemDelegateName)
+		throws Exception {
 
-			ObjectDefinition objectDefinition =
-				_objectDefinitionLocalService.getObjectDefinition(
-					entries.getKey());
+		if (ListUtil.isEmpty(ids) || ArrayUtil.isEmpty(permissions)) {
+			return Collections.emptyMap();
+		}
 
-			List<Map<String, Long>> list = new ArrayList<>();
+		ImportTask importTask = importTaskResource.putImportTaskObject(
+			className, null, null, null, taskItemDelegateName, "PARTIAL_UPDATE",
+			transform(
+				ids,
+				id -> HashMapBuilder.<String, Object>put(
+					"id", id
+				).put(
+					"permissions",
+					transformToList(
+						permissions,
+						permission -> HashMapBuilder.<String, Object>put(
+							"actionIds",
+							ListUtil.fromArray(permission.getActionIds())
+						).put(
+							"roleExternalReferenceCode",
+							permission.getRoleExternalReferenceCode()
+						).put(
+							"roleName", permission.getRoleName()
+						).put(
+							"roleType", permission.getRoleType()
+						).build())
+				).build()));
 
-			for (Long id : entries.getValue()) {
-				list.add(
-					HashMapBuilder.put(
-						"id", id
-					).build());
+		return HashMapBuilder.<String, Object>put(
+			"id", importTask.getId()
+		).put(
+			"processedItems", ids
+		).build();
+	}
+
+	private List<Map<String, Object>> _removeEntries(
+			Map<String, Map<Long, List<Long>>> map)
+		throws Exception {
+
+		List<Map<String, Object>> lmap = new ArrayList<>();
+		ImportTaskResource importTaskResource = _getImportTaskResource();
+
+		if (map.containsKey("ObjectEntry")) {
+			Map<Long, List<Long>> objectEntries = map.get("ObjectEntry");
+
+			for (Map.Entry<Long, List<Long>> entry : objectEntries.entrySet()) {
+				ObjectDefinition objectDefinition =
+					_objectDefinitionLocalService.getObjectDefinition(
+						entry.getKey());
+
+				lmap.add(
+					_deleteImportTaskObject(
+						"com.liferay.object.rest.dto.v1_0.ObjectEntry",
+						entry.getValue(), importTaskResource,
+						objectDefinition.getName()));
 			}
+		}
 
-			ImportTask importTask = importTaskResource.deleteImportTaskObject(
-				"com.liferay.object.rest.dto.v1_0.ObjectEntry", null, null,
-				ImportTask.ImportStrategy.ON_ERROR_CONTINUE.getValue(),
-				objectDefinition.getName(), list);
+		if (map.containsKey("ObjectEntryFolder")) {
+			Map<Long, List<Long>> objectEntryFolders = map.get(
+				"ObjectEntryFolder");
 
-			batchIds.add(importTask.getId());
+			lmap.add(
+				_deleteImportTaskObject(
+					"com.liferay.headless.object.dto.v1_0.ObjectEntryFolder",
+					ListUtil.fromMapKeys(objectEntryFolders),
+					importTaskResource, null));
+		}
+
+		return lmap;
+	}
+
+	private List<Map<String, Object>> _setPermissions(
+			Map<String, Map<Long, List<Long>>> entriesIdByObjectDefinition,
+			Permission[] permissions)
+		throws Exception {
+
+		if (ArrayUtil.isEmpty(permissions)) {
+			return Collections.emptyList();
+		}
+
+		ImportTaskResource importTaskResource = _getImportTaskResource();
+		List<Map<String, Object>> batchIds = new ArrayList<>();
+
+		if (entriesIdByObjectDefinition.containsKey("ObjectEntry")) {
+			Map<Long, List<Long>> objectEntries =
+				entriesIdByObjectDefinition.get("ObjectEntry");
+
+			for (Map.Entry<Long, List<Long>> entries :
+					objectEntries.entrySet()) {
+
+				ObjectDefinition objectDefinition =
+					_objectDefinitionLocalService.getObjectDefinition(
+						entries.getKey());
+
+				batchIds.add(
+					_putImportTaskObject(
+						"com.liferay.object.rest.dto.v1_0.ObjectEntry",
+						entries.getValue(), importTaskResource, permissions,
+						objectDefinition.getName()));
+			}
+		}
+
+		if (entriesIdByObjectDefinition.containsKey("ObjectEntryFolder")) {
+			Map<Long, List<Long>> objectEntryFolders =
+				entriesIdByObjectDefinition.get("ObjectEntryFolder");
+
+			batchIds.add(
+				_putImportTaskObject(
+					"com.liferay.headless.object.dto.v1_0.ObjectEntryFolder",
+					ListUtil.fromMapKeys(objectEntryFolders),
+					importTaskResource, permissions, null));
 		}
 
 		return batchIds;
@@ -140,6 +279,9 @@ public class ObjectEntryCMSBulkActionResourceImpl
 
 	@Reference
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryFolderLocalService _objectEntryFolderLocalService;
 
 	@Reference
 	private ObjectEntryLocalService _objectEntryLocalService;
